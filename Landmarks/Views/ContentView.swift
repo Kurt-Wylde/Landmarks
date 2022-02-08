d79f333b7b16f44ced098ca32fcc8b32078b1cb8
Evgeny Koshkin <wylde.kurt@gmail.com>
2022-02-08 17:05:12 +0300

    stable version

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Evgeny Koshkin on 07.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
