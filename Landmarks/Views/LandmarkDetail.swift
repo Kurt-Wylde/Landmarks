58d0306786804e47e7eac99c1c47d38f58149dd3
Evgeny Koshkin <wylde.kurt@gmail.com>
2022-02-10 14:38:23 +0300

    added environmentalObject

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -9,28 +9,31 @@ import SwiftUI
 
 struct LandmarkDetail: View {
     var landmark: Landmark
-    
+
     var body: some View {
         ScrollView {
             MapView(coordinate: landmark.locationCoordinate)
                 .ignoresSafeArea(edges: .top)
                 .frame(height: 300)
+
             CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
+
             VStack(alignment: .leading) {
-                    Text(landmark.name)
-                        .font(.title)
+                Text(landmark.name)
+                    .font(.title)
+
                 HStack {
                     Text(landmark.park)
-                        .font(.subheadline)
                     Spacer()
                     Text(landmark.state)
-                        .font(.subheadline)
                 }
                 .font(.subheadline)
                 .foregroundColor(.secondary)
+
                 Divider()
+
                 Text("About \(landmark.name)")
                     .font(.title2)
                 Text(landmark.description)
@@ -44,6 +47,6 @@ struct LandmarkDetail: View {
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[0])
+        LandmarkDetail(landmark: ModelData().landmarks[0])
     }
 }
